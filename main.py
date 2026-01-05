6cb42f4e15504e2fa3a1f1f70e372f3cba77083c
Sai Yashasvini <155720691+saiyashasvinif3@users.noreply.github.com>
2026-01-05 16:48:06 +0530

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -14,7 +13,7 @@ class CUSTOMER_RETENTION:
         self.df['TotalCharges'] = pd.to_numeric(self.df['TotalCharges'], errors='coerce')
         print(self.df['TotalCharges'].dtype)
         print(self.df.info())
-
+       # Bar chart visualiation for churn count
         self.df['Churn']= self.df['Churn'].map({'Yes':1, 'No': 0})
         churn_count = self.df['Churn'].value_counts().sort_index()
         plt.figure(figsize=(6,4))
@@ -26,13 +25,14 @@ class CUSTOMER_RETENTION:
         plt.bar_label(plt.gca().containers[0])
         plt.show()
         plt.savefig("churn_distribution.png")
-
+        
+       #Box plot for monthly charges
         plt.figure(figsize=(6,4))
         plt.boxplot(self.df['MonthlyCharges'])
         plt.ylabel('Monthly Charges')
         plt.show()
         plt.savefig("monthly_charges_boxplot.png")
-
+      #Histogram for tenture 
         plt.figure(figsize=(6,4))
         plt.hist(self.df['tenure'], bins=30, edgecolor='k')
         plt.xlabel('Tenure (months)')
@@ -41,6 +41,7 @@ class CUSTOMER_RETENTION:
         plt.show()
         plt.savefig("tenure_distribution.png")
 
+        #Scatter plot for monthly charges vs total charges
         plt.figure(figsize=(6,4))
         plt.scatter(self.df['MonthlyCharges'], self.df['TotalCharges'], alpha=0.5)
         plt.xlabel('Monthly Charges')
@@ -49,12 +50,14 @@ class CUSTOMER_RETENTION:
         plt.show()
         plt.savefig("monthly_vs_total_charges.png")
 
+        #Piechart for knowing which internet service is observed more
         plt.figure(figsize=(6,4))
         plt.pie(self.df['InternetService'].value_counts(), labels=self.df['InternetService'].value_counts().index, autopct='%1.1f%%')
         plt.title('Internet Service Distribution')
         plt.show()
         plt.savefig("internet_service_distribution.png")
-
+        
+        #Heatmap for visualizing correlation between all the numerical columns
         plt.figure(figsize=(6,4))
         numeric_df = self.df.select_dtypes(include=['int64', 'float64'])
         sns.heatmap(numeric_df.corr(method='pearson'), annot=True, fmt=".2f", cmap='coolwarm')
@@ -62,19 +65,21 @@ class CUSTOMER_RETENTION:
         plt.show()
         plt.savefig("correlation_heatmap.png")
 
+        #countplot to visualizing which gender causes more churn
         plt.figure(figsize=(6,4))
         sns.countplot(x=self.df['gender'], hue=self.df['Churn'])
         plt.title("Gender Distribution by churn")
         plt.show()
         plt.savefig("gender_churn_distribution.png")
 
+        #countplot to whethere senior citien cause more churn
         plt.figure(figsize=(6,4))
         sns.countplot(x=self.df['SeniorCitizen'], hue=self.df['Churn'])
         plt.title("Senior Citizen Distribution by churn")
         plt.show()
         plt.savefig("senior_citizen_churn_distribution.png")
 
-        
+        # Countplot to visualize which internet service causes more churn
         plt.figure(figsize=(7,5))
         sns.countplot(x='InternetService', hue='Churn', data=self.df)
         plt.title("Internet Service vs Churn")
@@ -86,4 +91,5 @@ class CUSTOMER_RETENTION:
 if __name__ == "__main__":
     data = "WA_Fn-UseC_-Telco-Customer-Churn.csv"
     obj =  CUSTOMER_RETENTION(data)
+
     
