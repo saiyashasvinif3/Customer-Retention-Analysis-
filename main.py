b53df625138a775cb60a77847997c509bd8b8d85
Sai Yashasvini <155720691+saiyashasvinif3@users.noreply.github.com>
2026-01-06 22:08:17 +0530

    main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class CUSTOMER_RETENTION:
         self.df['Churn']= self.df['Churn'].map({'Yes':1, 'No': 0})
         churn_count = self.df['Churn'].value_counts().sort_index()
         plt.figure(figsize=(6,4))
-        plt.bar(churn_count.index,churn_count.values)
+        plt.bar(churn_count.index,churn_count.values, color=['blue','orange'])
         plt.xlabel('Churn')
         plt.ylabel('Count')
         plt.title('Churn Distribution')
@@ -24,30 +24,52 @@ class CUSTOMER_RETENTION:
         plt.bar_label(plt.gca().containers[0])
         plt.savefig("churn_distribution.png")
         plt.show()
-        
-        #Box plot for monthly charges
+        # Countplot to visualize senior citizen churn distribution
         plt.figure(figsize=(6,4))
-        plt.boxplot(self.df['MonthlyCharges'])
-        plt.ylabel('Monthly Charges')
-        plt.savefig("monthly_charges_boxplot.png")
+        ax = sns.countplot( x=self.df['SeniorCitizen'], hue=self.df['Churn'])
+        plt.title("Citizens Distribution by Churn")
+        plt.xlabel("Senior Citizen")
+        plt.ylabel("Count")
+        for container in ax.containers:
+           ax.bar_label(container, label_type='edge')
+        plt.xticks([0, 1], ['Non-Senior Citizen', 'Senior Citizen'])
+        plt.savefig("senior_citizen_churn_distribution.png")
         plt.show()
-        
-        #Histogram for tenure 
+        #barplot for gender churn analysis
+        churned = self.df[self.df['Churn'] == 1]
+        gender_counts = churned['gender'].value_counts() 
         plt.figure(figsize=(6,4))
-        plt.hist(self.df['tenure'], bins=30, edgecolor='k')
-        plt.xlabel('Tenure (months)')
-        plt.ylabel('Number of Customers')
-        plt.title('Distribution of Customer Tenure')
-        plt.savefig("tenure_distribution.png")
+        plt.bar(gender_counts.index, gender_counts.values, color=['skyblue', 'salmon']) # individual colors)
+        plt.title("Churned Customers by Gender")
+        plt.xlabel("Gender")
+        plt.ylabel("Number of Churned Customers")
+        plt.bar_label(plt.gca().containers[0])
+        plt.savefig("gender_chruned_customers.png")
+        plt.show()
+       
+       # Filter churned senior citizens
+        senior_churned = self.df[(self.df['SeniorCitizen'] == 1) & (self.df['Churn'] == 1)]
+        gender_counts = senior_churned['gender'].value_counts()
+        plt.figure(figsize=(6,4))
+        plt.bar(gender_counts.index,gender_counts.values,color=['skyblue', 'salmon'])
+        plt.title("Churned Senior Citizens by Gender")
+        plt.xlabel("Gender")
+        plt.ylabel("Number of Churned Senior Citizens")
+        plt.bar_label(plt.gca().containers[0])
+        plt.savefig("churned_senior_gender.png")
         plt.show()
 
-        #Scatter plot for monthly charges vs total charges
-        plt.figure(figsize=(6,4))
-        plt.scatter(self.df['MonthlyCharges'], self.df['TotalCharges'], alpha=0.5)
-        plt.xlabel('Monthly Charges')
-        plt.ylabel('Total Charges')
-        plt.title('Monthly Charges vs Total Charges')
-        plt.savefig("monthly_vs_total_charges.png")
+        #barplot to visualize sim usage by gender among senior citizens
+        senior_df = self.df[self.df['SeniorCitizen'] == 1]
+        sim_counts = senior_df.groupby(['gender', 'sim']).size().reset_index(name='count')
+        plt.figure(figsize=(10,6))
+        sns.barplot(data=sim_counts, x='sim', y='count', hue='gender', palette='Set2')
+        plt.title("SIM Usage by Gender in Senior Citizens")
+        plt.xlabel("SIM Provider")
+        plt.ylabel("Number of Users")
+        for container in plt.gca().containers:
+            plt.bar_label(container)
+        plt.savefig("senior_citizen_sim_usage.png")    
         plt.show()
 
         #Piechart for knowing which internet service is observed more
@@ -57,40 +79,133 @@ class CUSTOMER_RETENTION:
         plt.savefig("internet_service_distribution.png")
         plt.show()
         
-        #Heatmap for visualizing correlation between all the numerical columns
         plt.figure(figsize=(6,4))
-        numeric_df = self.df.select_dtypes(include=['int64', 'float64'])
-        sns.heatmap(numeric_df.corr(method='pearson'), annot=True, fmt=".2f", cmap='coolwarm')
-        plt.title('Correlation Heatmap')
-        plt.savefig("correlation_heatmap.png")
+        plt.pie(self.df['sim'].value_counts(), labels=self.df['sim'].value_counts().index, autopct='%1.1f%%')
+        plt.title('SIM Distribution')
+        plt.savefig("sim_distribution.png")
+        plt.show()
+        
+        plt.figure(figsize=(6,4))
+        plt.pie(self.df['Contract'].value_counts(), labels=self.df['Contract'].value_counts().index, autopct='%1.1f%%')
+        plt.title('Contract Distribution')
+        plt.savefig("contract_distribution.png")
+        plt.show()
+      
+        internetcounts = self.df.groupby(['InternetService', 'Churn']).size().reset_index(name='count')
+        plt.figure(figsize=(10,6))
+        ax = sns.barplot(data=internetcounts, x='InternetService', y='count', hue='Churn', palette='Set2')
+        plt.title("Internet Service vs Churn Distribution")
+        plt.xlabel("Internet Service Provider")
+        plt.ylabel("Number of Users")
+        for container in ax.containers:
+            ax.bar_label(container)
+        plt.savefig("internetservice_churn_distribution.png")
         plt.show()
 
-        #countplot to visualizing which gender causes more churn
-        plt.figure(figsize=(6,4))
-        sns.countplot(x=self.df['gender'], hue=self.df['Churn'])
-        plt.title("Gender Distribution by churn")
-        plt.savefig("gender_churn_distribution.png")
+        contractcounts = self.df.groupby(['Contract', 'Churn']).size().reset_index(name='count')
+        plt.figure(figsize=(10,6))
+        ax = sns.barplot(data=contractcounts, x='Contract', y='count', hue='Churn', palette='Set2')
+        plt.title("Contract vs Churn Distribution")
+        plt.xlabel("Contract Type")
+        plt.ylabel("Number of Users")
+        for container in ax.containers:
+            ax.bar_label(container)
+        plt.savefig("contract_churn_distribution.png")
         plt.show()
 
-        #countplot to whethere senior citizen cause more churn
-        plt.figure(figsize=(6,4))
-        sns.countplot(x=self.df['SeniorCitizen'], hue=self.df['Churn'])
-        plt.title("Senior Citizen Distribution by churn")
-        plt.savefig("senior_citizen_churn_distribution.png")
+        simcounts = self.df.groupby(['sim', 'Churn']).size().reset_index(name='count')
+        plt.figure(figsize=(10,6))
+        ax = sns.barplot(data=simcounts, x='sim', y='count', hue='Churn', palette='Set2')
+        plt.title("SIM vs Churn Distribution")
+        plt.xlabel("SIM Provider")
+        plt.ylabel("Number of Users")
+        for container in ax.containers:
+            ax.bar_label(container)
+        plt.savefig("sim_churn_distribution.png")
         plt.show()
 
-        # Countplot to visualize which internet service causes more churn
-        plt.figure(figsize=(7,5))
-        sns.countplot(x='InternetService', hue='Churn', data=self.df)
-        plt.title("Internet Service vs Churn")
-        plt.xlabel("Internet Service")
+     
+
+# Count of customers for each tenure value
+        tenurecounts = self.df['tenure'].value_counts().sort_index()
+        plt.figure(figsize=(10,6))
+        plt.plot(tenurecounts.index, tenurecounts.values, marker='o', color='blue', linewidth=2)
+# Add labels and title
+        plt.xlabel("Tenure (Months)")
         plt.ylabel("Number of Customers")
-        plt.savefig("internet_service_churn_distribution.png")
+        for container in ax.containers:
+            ax.bar_label(container)
+        plt.title("Customer Distribution by Tenure")
+        plt.grid(True)
+        plt.savefig("tenure_line_chart.png")
+        plt.show()
+
+        plt.figure(figsize=(6,4))
+        techcounts = self.df.groupby(['TechSupport', 'Churn']).size().unstack()
+        ax=techcounts.plot(kind='bar', figsize=(8,5), color=['skyblue','salmon'])
+        plt.xlabel("Tech Support Subscription")
+        plt.ylabel("Number of Customers")
+        for container in ax.containers:
+            ax.bar_label(container)
+        plt.title("Influence of Tech Support on Churn")
+        plt.xticks(rotation=0) 
+        plt.legend(title='Churn')
+        plt.savefig("techsupport_churn_distribution_simple.png")
+        plt.show()
+       
+
+        plt.figure(figsize=(6,4))
+        tech_counts = self.df.groupby(['TechSupport', 'sim']).size().unstack()
+        ax=tech_counts.plot(kind='bar', figsize=(8,5), color=['skyblue','salmon','lightgreen'])
+        plt.xlabel("Tech Support Subscription")
+        plt.ylabel("Number of Customers")
+        for container in ax.containers:
+            ax.bar_label(container)
+        plt.title("Influence of Tech Support on SIM ")
+        plt.xticks(rotation=0) 
+        plt.legend(title='SIM')
+        plt.savefig("techsupport_sim_distribution.png")
+        plt.show()
+       
+        monthlycharge = self.df.groupby('Churn')['MonthlyCharges'].mean()
+        plt.figure(figsize=(6,4))
+        plt.plot(monthlycharge.index, monthlycharge.values, marker='o')
+        plt.xlabel('Churn (0 = No, 1 = Yes)')
+        plt.ylabel('Average Monthly Charges')
+        plt.title('Impact of Monthly Charges on Churn')
+        plt.grid(True)
+        plt.savefig("monthly_charges_vs_churn.png")
+        plt.show()
+        
+        tenurecount = self.df['tenure'].value_counts().sort_index()
+        plt.figure(figsize=(7,4))
+        plt.fill_between(tenurecount.index, tenurecount.values, alpha=0.6)
+        plt.xlabel('Tenure (Months)')
+        plt.ylabel('Number of Customers')
+        plt.title('Customer Distribution by Tenure')
+        plt.savefig("tenure_area_chart.png")
         plt.show()
 
 if __name__ == "__main__":
-    data = "WA_Fn-UseC_-Telco-Customer-Churn.csv"
+    data = "Telco_Churn.csv"
     obj =  CUSTOMER_RETENTION(data)
+
+    
+
+
+
+
+
+    
+
+
+
+
+
+    
+
+
+
 
     
 
